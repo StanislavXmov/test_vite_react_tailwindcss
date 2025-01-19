d124ddd961cf9a097605bf35dfd124e4e8a3de54
stanislav <xiiiovart@gmail.com>
2025-01-19 12:18:11 +0300

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="font-mono underline">vite react ts tailwindcss</h1>
+      <Button>TEST</Button>
     </div>
   )
 }
